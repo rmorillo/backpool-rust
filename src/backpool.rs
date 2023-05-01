36339aec4cf550290a51bf49ef45c00fa94af2a0
Ronaldo Morillo <ronaldo.morillo@yahoo.com>
2023-05-01 15:07:10 +0800

    Added unit testing

--- src/backpool.rs
+++ src/backpool.rs
@@ -27,3 +27,28 @@ impl<T> LookBehindPool<T>{
 
 }
 
+#[cfg(test)]
+
+struct Prices{
+    open: u64,
+    high: u64,
+    low: u64,
+    close: u64
+}
+
+#[test]
+fn it_works() {
+    let input: usize = 0;
+
+    println!("value={}",input);
+    let mut my_thing = LookBehindPool::<Prices>::new(2);
+    my_thing.write(Prices{open: 1, high:2, low:3, close: 4});
+    my_thing.write(Prices{open: 1, high:6, low:3, close: 4});
+    //my_thing.write(Prices{open: 1, high:5, low:3, close: 4});
+    println!("the thing's total is {}", my_thing.item(input).open);
+    println!("the thing's total is {}", my_thing.item(input).high);
+    println!("the thing's total is {}", my_thing.item(input).low);
+    println!("the thing's total is {}", my_thing.item(input).close);
+
+    assert_eq!(2 + 2, 4);
+}
